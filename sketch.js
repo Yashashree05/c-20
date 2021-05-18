ec87e3058ec0d149bd8eeeacaf3d53a289bdc70f
Yashashree05 <82882573+Yashashree05@users.noreply.github.com>
2021-05-18 20:04:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var a,b 
+
+
+function setup() {
+  createCanvas(800,800);
+
+
+  a = createSprite(400, 200, 40,  40);
+  b = createSprite(200, 200, 50, 50);
+
+a.shapeColor = "lime"
+b.shapeColor = "lime"
+
+}
+
+function draw() {
+
+  background("lightblue"); 
+
+a.x = World.mouseX
+a.y = World.mouseY
+  
+if(a.x-b.x<a.width/2 + b.width/2 && b.x-a.x<a.width/2 + b.width/2 && a.y-b.y<a.height/2 + b.height/2 && b.y-a.y<a.height/2 + b.height/2 ){
+  a.shapeColor = "red"
+  b.shapeColor = "red"
+} else {
+  a.shapeColor = "lime"
+b.shapeColor = "lime"
+}
+
+  drawSprites();
+}
